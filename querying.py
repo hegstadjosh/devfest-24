26a070b835f30786fcebfbda5e0b0abef809f51a
jamesyu13 <82225616+jamesyu13@users.noreply.github.com>
2024-02-03 20:06:28 -0500

    Update querying.py

--- querying.py
+++ querying.py
@@ -1,6 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import ai_implementation
 
 # Initialize Spotify API credentials
 
@@ -9,23 +9,31 @@ CLIENT_SECRET = '97d7cb45f17743d2a20ba1be4b521ace'
 
 URI = 'http://localhost:3000'
 
-# Initialize sentiment analyzer
-sia = SentimentIntensityAnalyzer()
+sentiments = ai_implementation.runJournal()
 
 # Initialize Spotipy with Client Credentials Flow
 auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-query = ['angry', 'happiness', 'satisfaction']  # Example query related to mental health podcasts
-query = ' '.join(query)
-print(query)
-# Search for podcasts on Spotify
-results = sp.search(q=query, type='episode', market = 'US', limit = 3)
 
-results = results['episodes']['items']
+first3 = list(sentiments.keys())[-3:]
+last3 =  list(sentiments.keys())[:3]
 
-for podcast in results:
-    print(f"Title: {podcast['name']}")
-    print(f"Description: {podcast['description']}")
-    print(f"Listen here: {podcast['external_urls']['spotify']}")
-    print()
+def getQuery(query):
+    query = ' '.join(query)
+    print('')
+    # Search for podcasts on Spotify
+    results = sp.search(q=query, type='episode', market = 'US', limit = 3)
+    
+    results = results['episodes']['items']
+    
+    for podcast in results:
+        print(f"Title: {podcast['name']}")
+        print(f"Description: {podcast['description']}")
+        print(f"Listen here: {podcast['external_urls']['spotify']}")
+        print()
+
+print("Podcasts recommended based on your feelings now")
+getQuery(first3)
+print("You might also like: ")
+getQuery(last3)
