c623231d47b5f0500a76a163b1d4040aa6752e16
jamesyu13 <82225616+jamesyu13@users.noreply.github.com>
2024-02-03 21:10:32 -0500

    Update querying.py

--- querying.py
+++ querying.py
@@ -10,30 +10,31 @@ CLIENT_SECRET = '97d7cb45f17743d2a20ba1be4b521ace'
 URI = 'http://localhost:3000'
 
 sentiments = ai_implementation.runJournal()
+print(sentiments)
 
 # Initialize Spotipy with Client Credentials Flow
 auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-
-first3 = list(sentiments.keys())[-3:]
-last3 =  list(sentiments.keys())[:3]
-
-def getQuery(query):
-    query = ' '.join(query)
-    print('')
-    # Search for podcasts on Spotify
-    results = sp.search(q=query, type='episode', market = 'US', limit = 3)
+if sentiments:
+    first3 = list(sentiments.keys())[-3:]
+    last3 =  list(sentiments.keys())[:3]
     
-    results = results['episodes']['items']
+    def getQuery(query):
+        query = ' '.join(query)
+        print('')
+        # Search for podcasts on Spotify
+        results = sp.search(q=query, type='episode', market = 'US', limit = 3)
+        
+        results = results['episodes']['items']
+        
+        for podcast in results:
+            print(f"Title: {podcast['name']}")
+            print(f"Description: {podcast['description']}")
+            print(f"Listen here: {podcast['external_urls']['spotify']}")
+            print()
     
-    for podcast in results:
-        print(f"Title: {podcast['name']}")
-        print(f"Description: {podcast['description']}")
-        print(f"Listen here: {podcast['external_urls']['spotify']}")
-        print()
-
-print("Podcasts recommended based on your feelings now")
-getQuery(first3)
-print("You might also like: ")
-getQuery(last3)
+    print("\n Podcasts recommended based on your feelings now")
+    getQuery(first3)
+    print("You might also like: ")
+    getQuery(last3)
